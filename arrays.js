9cb68e75597d3471923b9779c1206e7a0d8b3190
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-05-09 15:18:49 -0400

    Create arrays.js
    How arrays works in javascript and what is core difference in them in JAVA

---
+++ arrays.js
@@ -0,0 +1,6 @@
+// we will seee how arrays here work likes as same as we used stack syetem in java ( last in - First out ) 
+
+ let myCars = ["M8 competition","RR", "AMG C63"];
+myCars.push("G63");
+console.log(cars);
+
