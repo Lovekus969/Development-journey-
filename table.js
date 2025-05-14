405c2fb5947b16d44affed106fcfec791756c5b9
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-05-14 10:38:56 -0400

    Create table.js

---
+++ table.js
@@ -0,0 +1,7 @@
+// Can you write the table for 75 untill 20 
+function table(n){// n is parameter where i will passed 75 in the calling method 
+  for(  let i=n; i<=n*20;i+=n){
+        console.log(i);
+  }
+}
+table(75);
