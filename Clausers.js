5e11cd821d1eb6ac177e65b07caf2a494c6330ae
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-05-22 22:29:16 -0400

    Update Clausers.js
    . Count Frequency of Elements
    Input: ['apple', 'banana', 'apple']
    Goal: { apple: 2, banana: 1 }

--- Clausers.js
+++ Clausers.js
@@ -4,3 +4,15 @@ function demo() {
   x = 7;
   console.log(x);   // 7
 }
+
+const fruits = ['apple', 'banana', 'apple'];
+
+const count = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(count); //  { apple: 2, banana: 1 }
+//. Count Frequency of Elements
+// Input: ['apple', 'banana', 'apple']
+// Goal: { apple: 2, banana: 1 }
