fd34ba1d545b8ae0cff4041040c897deac133c1d
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-06-01 10:50:28 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int speed = 250;
+        double enginePower = 617.5;
+        char grade = 'A';
+        boolean isRunning = true;
+        String carModel = "BMW M8";
+
+        System.out.println("Speed: " + speed);
+        System.out.println("Engine Power: " + enginePower);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Running: " + isRunning);
+        System.out.println("Car Model: " + carModel);
+    }
+}
