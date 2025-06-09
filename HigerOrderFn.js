bda295a5ac00a9b3fcfd19a283aae92e0e149b72
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-06-09 08:22:26 -0400

    Update HigerOrderFn.js

--- HigerOrderFn.js
+++ HigerOrderFn.js
@@ -9,4 +9,11 @@ function sayHello() {
 }
 
 console.log(hof(sayHello)); // Output: Hello from FAANG!
+//Examples of Higher-Order Functions in JavaScript:
+
+const nums = [1, 2, 3];
+const doubled = nums.map(n => n * 2); // [2, 4, 6]  // Map()
+//use arrow functions when mapping in JavaScript.
+//Arrow functions offer a shorter and cleaner syntax, especially useful in functional methods like map, filter, reduce, etc.
+
 
